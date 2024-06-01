6d617c1b47e1eea3c30931baaeef1ef04789ffcc
Allan Ritchie <allan.ritchie@gmail.com>
2024-05-31 23:19:37 -0400

    Notes

--- src/Shiny.Mediator/IRequestHandler.cs
+++ src/Shiny.Mediator/IRequestHandler.cs
@@ -1,12 +1,12 @@
 namespace Shiny.Mediator;
 
-public interface IRequestHandler<in TRequest> where TRequest : IRequest
+public interface IRequestHandler<in TRequest> where TRequest : notnull, IRequest
 {
     Task Handle(TRequest request, CancellationToken cancellationToken);
 }
 
 
-public interface IRequestHandler<in TRequest, TResult> where TRequest : IRequest<TResult>
+public interface IRequestHandler<in TRequest, TResult> where TRequest : notnull, IRequest<TResult>
 {
     Task<TResult> Handle(TRequest request, CancellationToken cancellationToken);
 }
--- tests/Shiny.Mediator.Tests/SourceGeneratorTests.cs
+++ tests/Shiny.Mediator.Tests/SourceGeneratorTests.cs
@@ -1,42 +1,43 @@
-// using FluentAssertions;
-// using Shiny.Mediator;
-//
-// namespace Shiny.Mediator.Tests;
-//
-// public class SourceGeneratorTests
-// {
-//     [Fact]
-//     public void DidRegister()
-//     {
-//         var services = new ServiceCollection();
-//         services.AddShinyMediator();
-//         // TODO: source gen call
-//         var sp = services.BuildServiceProvider();
-//
-//         sp.GetService<IEventHandler<SourceGenEvent>>().Should().NotBeNull("Event Handler not found");
-//         sp.GetService<IRequestHandler<SourceGenRequest>>().Should().NotBeNull("Request Handler not found");
-//         sp.GetService<IRequestHandler<SourceGenResponseRequest, SourceGenResponse>>().Should().NotBeNull("Request/Response Handler not found");
-//     }
-// }
-//
-// public record SourceGenRequest : IRequest;
-// public record SourceGenResponseRequest : IRequest<SourceGenResponse>;
-// public record SourceGenResponse;
-// public record SourceGenEvent : IEvent;
-//
-// [RegisterHandler]
-// public class SourceGenRequestHandler : IRequestHandler<SourceGenRequest>
-// {
-//     public Task Handle(SourceGenRequest request, CancellationToken cancellationToken) => Task.CompletedTask;
-// }
-// [RegisterHandler]
-// public class SourceGenResponseRequestHandler : IRequestHandler<SourceGenResponseRequest, SourceGenResponse>
-// {
-//     public Task<SourceGenResponse> Handle(SourceGenResponseRequest request, CancellationToken cancellationToken)
-//         => Task.FromResult(new SourceGenResponse());
-// }
-// [RegisterHandler]
-// public class SourceGenEventHandler : IEventHandler<SourceGenEvent>
-// {
-//     public Task Handle(SourceGenEvent @event, CancellationToken cancellationToken) => Task.CompletedTask;
-// }
+using FluentAssertions;
+using Shiny.Mediator;
+
+namespace Shiny.Mediator.Tests;
+
+public class SourceGeneratorTests
+{
+    [Fact]
+    public void DidRegister()
+    {
+        var services = new ServiceCollection();
+        services.AddShinyMediator();
+        // TODO: source gen call
+        // services.AddDiscoveredMediatorHandlers();
+        var sp = services.BuildServiceProvider();
+
+        sp.GetService<IEventHandler<SourceGenEvent>>().Should().NotBeNull("Event Handler not found");
+        sp.GetService<IRequestHandler<SourceGenRequest>>().Should().NotBeNull("Request Handler not found");
+        sp.GetService<IRequestHandler<SourceGenResponseRequest, SourceGenResponse>>().Should().NotBeNull("Request/Response Handler not found");
+    }
+}
+
+public record SourceGenRequest : IRequest;
+public record SourceGenResponseRequest : IRequest<SourceGenResponse>;
+public record SourceGenResponse;
+public record SourceGenEvent : IEvent;
+
+[RegisterHandler]
+public class SourceGenRequestHandler : IRequestHandler<SourceGenRequest>
+{
+    public Task Handle(SourceGenRequest request, CancellationToken cancellationToken) => Task.CompletedTask;
+}
+[RegisterHandler]
+public class SourceGenResponseRequestHandler : IRequestHandler<SourceGenResponseRequest, SourceGenResponse>
+{
+    public Task<SourceGenResponse> Handle(SourceGenResponseRequest request, CancellationToken cancellationToken)
+        => Task.FromResult(new SourceGenResponse());
+}
+[RegisterHandler]
+public class SourceGenEventHandler : IEventHandler<SourceGenEvent>
+{
+    public Task Handle(SourceGenEvent @event, CancellationToken cancellationToken) => Task.CompletedTask;
+}
