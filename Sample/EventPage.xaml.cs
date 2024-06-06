e9ae2fd9d6d080f0b386694a7539671e078a626f
Allan Ritchie <allan.ritchie@gmail.com>
2024-06-05 23:47:18 -0400

    Starting on cache unit tests + some cleanup

--- Sample/EventPage.xaml.cs
+++ Sample/EventPage.xaml.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Sample;
 
 public partial class EventPage : ContentPage
 {
     public EventPage()
     {
-        InitializeComponent();
+        this.InitializeComponent();
     }
 }
--- tests/Shiny.Mediator.Tests/CacheRequestMiddlewareTests.cs
+++ tests/Shiny.Mediator.Tests/CacheRequestMiddlewareTests.cs
@@ -1,6 +1,71 @@
+using Microsoft.Extensions.Configuration;
+using Shiny.Mediator.Middleware;
+
 namespace Shiny.Mediator.Tests;
 
 public class CacheRequestMiddlewareTests
 {
-    
+    [Fact]
+    public async Task EndToEnd()
+    {
+        var conn = new MockConnectivity();
+        var fs = new MockFileSystem();
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(new Dictionary<string, string>
+            {
+                ["Cache:Shiny.Mediator.Tests.CacheRequest"] = ""
+            })
+            .Build();
+        
+        var middleware = new CacheRequestMiddleware<CacheRequest, CacheResponse>(configuration, conn, fs);
+
+        // TODO: test with ICacheItem
+        var request = new CacheRequest();
+
+        var result1 = await middleware.Process(
+            request,
+            () => Task.FromResult(
+                new CacheResponse(DateTimeOffset.UtcNow.Ticks)
+            ), 
+            CancellationToken.None
+        );
+        await Task.Delay(2000);
+        var result2 = await middleware.Process(
+            request,
+            () => Task.FromResult(
+                new CacheResponse(DateTimeOffset.UtcNow.Ticks)
+            ), 
+            CancellationToken.None
+        );
+        
+        // if cached
+        result1.Ticks.Should().Be(result2.Ticks);
+    }
+}
+
+
+public record CacheRequest : IRequest<CacheResponse>;
+public record CacheResponse(long Ticks);
+
+public class MockConnectivity : IConnectivity
+{
+    public IEnumerable<ConnectionProfile> ConnectionProfiles { get; set; }// = ConnectionProfile.WiFi;
+    public NetworkAccess NetworkAccess { get; set; }
+    public event EventHandler<ConnectivityChangedEventArgs>? ConnectivityChanged;
+}
+
+public class MockFileSystem : IFileSystem
+{
+    public Task<Stream> OpenAppPackageFileAsync(string filename)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> AppPackageFileExistsAsync(string filename)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string CacheDirectory { get; } = ".";
+    public string AppDataDirectory { get; } = ".";
 }
