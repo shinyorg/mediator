04a984c716e2389526d9232daf6c0a26e819c4dd
Allan Ritchie <allan.ritchie@gmail.com>
2024-06-25 21:24:07 -0400

    More improvements to request keys

---
+++ src/Shiny.Mediator.Contracts/IRequestKey.cs
@@ -0,0 +1,10 @@
+namespace Shiny.Mediator;
+
+/// <summary>
+/// This is viewed by replay, cache, and various other services where you can control an entry
+/// Simply mark your IRequest or IStreamRequest and provide the necessary key to determine uniqueness
+/// </summary>
+public interface IRequestKey
+{
+    string GetKey();
+}
