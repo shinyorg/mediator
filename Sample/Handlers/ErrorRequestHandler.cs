b2be7c74da0232b91034054534c1ca3606c0d28a
Allan Ritchie <allan.ritchie@gmail.com>
2024-06-29 13:42:11 -0400

    Update ErrorRequestHandler.cs

--- Sample/Handlers/ErrorRequestHandler.cs
+++ Sample/Handlers/ErrorRequestHandler.cs
@@ -6,6 +6,8 @@ namespace Sample.Handlers;
 [RegisterHandler]
 public class ErrorRequestHandler : IRequestHandler<ErrorRequest>
 {
+    
+    [UserNotify(ErrorTitle = "NOPE", ErrorMessage = "Quit causing issues :)")]
     public Task Handle(ErrorRequest request, CancellationToken cancellationToken)
     {
         throw new InvalidOperationException("Why you call me?");
