f1dee35ccfd03d1ee18a163e40e2bfdc7054554f
Allan Ritchie <allan.ritchie@gmail.com>
2024-08-25 13:34:02 -0400

    Start generator

---
+++ src/Shiny.Mediator.HttpRequestGenerator/ContractGenerator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.OpenApi.Readers;
+
+namespace Shiny.Mediator.HttpRequestGenerator;
+
+public class ContractGenerator
+{
+    public void Test(Stream stream)
+    {
+        using var streamReader = new StreamReader(stream);
+        var reader = new OpenApiStreamReader();
+        var document = reader.Read(streamReader.BaseStream, out var diagnostic);
+        
+        foreach (var path in document.Paths)
+        {
+            // path.Key
+            foreach (var op in path.Value.Operations)
+            {
+                foreach (var p in op.Value.Parameters)
+                {
+
+                }
+            }
+        }
+
+    }
+}
