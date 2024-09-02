1403984804dbec60b31715c2a489dd85b10f4bf2
Allan Ritchie <allan.ritchie@gmail.com>
2024-09-02 08:01:58 -0400

    add http parameters

--- src/Shiny.Mediator/Http/HttpAttribute.cs
+++ src/Shiny.Mediator/Http/HttpAttribute.cs
@@ -6,3 +6,19 @@ public class HttpAttribute(HttpMethod method, string route) : Attribute
     public HttpMethod Method => method;
     public string Route => route;
 }
+
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter, Inherited = false, AllowMultiple = false)]
+public class HttpParameterAttribute(HttpParameterType parameterType) : Attribute
+{
+    public HttpParameterType Type => parameterType;
+}
+
+public enum HttpParameterType
+{
+    Header,
+    Query,
+    // Cookie,
+    Path,
+    Body
+}
--- src/Shiny.Mediator/Http/HttpRequestHandler.cs
+++ src/Shiny.Mediator/Http/HttpRequestHandler.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using System.Text;
 using System.Text.Json;
 using Microsoft.Extensions.Configuration;
 
@@ -17,27 +18,71 @@ public class HttpRequestHandler<TRequest, TResult>(
         if (http == null)
             throw new InvalidOperationException("HttpAttribute not specified on request");
 
-        var baseUri = configuration.GetSection("Mediator:Http")?["BaseUri"];
-        var httpRequest = new HttpRequestMessage(http.Method, baseUri + http.Route);
+        var baseUri = http.Route.StartsWith("http")
+            ? http.Route
+            : configuration.GetSection("Mediator:Http")?["BaseUri"] ?? "http://localhost";
+        
+        var httpRequest = new HttpRequestMessage(http.Method, baseUri);
+        var properties = request
+            .GetType()
+            .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+            .ToList();
+
+        var uri = baseUri + http.Route;
+        foreach (var property in properties)
+        {
+            var parameter = property.GetCustomAttribute<HttpParameterAttribute>();
+            if (parameter != null)
+            {
+                var propertyValue = property.GetValue(request);
+                switch (parameter.Type)
+                {
+                    case HttpParameterType.Path:
+                    case HttpParameterType.Query:
+                        var value = propertyValue?.ToString() ?? String.Empty;
+                        uri = uri.Replace("{" + property.Name + "}", value);
+                        break;
+                
+                    case HttpParameterType.Header:
+                        if (propertyValue != null)
+                            httpRequest.Headers.Add(property.Name, propertyValue.ToString());
+                        break;
+                
+                    case HttpParameterType.Body:
+                        // TODO: file upload or form post?
+                        if (httpRequest.Content != null)
+                            throw new InvalidOperationException("Multiple body parameters not supported");
+                        
+                        var json = JsonSerializer.Serialize(propertyValue);
+                        httpRequest.Content = new StringContent(json, Encoding.UTF8, "application/json");
+                        break;
+                }
+            }
+        }
+
+        httpRequest.RequestUri = new Uri(uri);
         foreach (var decorator in decorators)
         {
             await decorator
                 .Decorate(httpRequest, request)
                 .ConfigureAwait(false);
         }
-    
+        
         var response = await this.httpClient
             .SendAsync(httpRequest, cancellationToken)
             .ConfigureAwait(false);
         
         response.EnsureSuccessStatusCode();
-        var stringResult = await response
-            .Content
-            .ReadAsStringAsync(cancellationToken)
-            .ConfigureAwait(false);
-        
-        var result = JsonSerializer.Deserialize<TResult>(stringResult);
-    
-        return result!;
+        TResult finalResult = default!;
+        if (typeof(TResult) != typeof(Unit))
+        {
+            var stringResult = await response
+                .Content
+                .ReadAsStringAsync(cancellationToken)
+                .ConfigureAwait(false);
+
+            finalResult = JsonSerializer.Deserialize<TResult>(stringResult)!;
+        }
+        return finalResult!;
     }
 }
