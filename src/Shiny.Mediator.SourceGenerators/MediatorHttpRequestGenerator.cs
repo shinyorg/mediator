ae4bac383c4d21060302d9b6152465607e573a8c
Allan Ritchie <allan.ritchie@gmail.com>
2024-09-05 00:04:59 -0400

    Fixes

--- src/Shiny.Mediator.SourceGenerators/MediatorHttpRequestGenerator.cs
+++ src/Shiny.Mediator.SourceGenerators/MediatorHttpRequestGenerator.cs
@@ -24,60 +24,52 @@ public class MediatorHttpRequestGenerator : ISourceGenerator
         context.LogInfo("HTTP Generator - Item Count: " + items.Length);
         foreach (var item in items)
         {
-            context
-                .AnalyzerConfigOptions
-                .GetOptions(item)
-                .TryGetValue("build_metadata.AdditionalFiles.Namespace", out var nameSpace);
-
-            nameSpace ??= rootNamespace;
-            
-            if (item.Path.StartsWith("http", StringComparison.InvariantCultureIgnoreCase))
-                this.Remote(context, item, nameSpace);
-            else
-                this.Local(context, item, nameSpace);
+            try
+            {
+                context
+                    .AnalyzerConfigOptions
+                    .GetOptions(item)
+                    .TryGetValue("build_metadata.AdditionalFiles.Namespace", out var nameSpace);
+                
+                nameSpace ??= rootNamespace;
+                if (item.Path.StartsWith("http", StringComparison.InvariantCultureIgnoreCase))
+                    this.Remote(context, item, nameSpace);
+                else
+                    this.Local(context, item, nameSpace);
+            }
+            catch (Exception ex)
+            {
+                context.LogError("Error Generating HTTP Contracts: " + ex);
+            }
         }
     }
 
     void Local(GeneratorExecutionContext context, AdditionalText item, string nameSpace)
     {
-        try
-        {
-            context.LogInfo($"Generating from local file '{item.Path}' with namespace '{nameSpace}'");
+        context.LogInfo($"Generating from local file '{item.Path}' with namespace '{nameSpace}'");
 
-            var localCode = item.GetText(context.CancellationToken)!.ToString();
-            var output = OpenApiContractGenerator.Generate(
-                new MemoryStream(Encoding.UTF8.GetBytes(localCode)),
-                nameSpace,
-                e => context.LogInfo(e)
-            );
-            context.AddSource(nameSpace + ".g.cs", output);
-        }
-        catch (Exception ex)
-        {
-            context.LogError("Error generating local contract: " + ex);
-        }
+        var localCode = item.GetText(context.CancellationToken)!.ToString();
+        var output = OpenApiContractGenerator.Generate(
+            new MemoryStream(Encoding.UTF8.GetBytes(localCode)),
+            nameSpace,
+            e => context.LogInfo(e)
+        );
+        context.AddSource(nameSpace + ".g.cs", output);
     }
 
 
     void Remote(GeneratorExecutionContext context, AdditionalText item, string nameSpace)
     {
-        try
-        {
-            var remoteUri = new Uri(item.Path);
-            context.LogInfo($"Generating for remote '{item.Path}' with namespace '{nameSpace}'");
-            var http = new HttpClient { BaseAddress = remoteUri };
-            var stream = http.GetStreamAsync(remoteUri).GetAwaiter().GetResult();
+        var remoteUri = new Uri(item.Path);
+        context.LogInfo($"Generating for remote '{item.Path}' with namespace '{nameSpace}'");
+        var http = new HttpClient { BaseAddress = remoteUri };
+        var stream = http.GetStreamAsync(remoteUri).GetAwaiter().GetResult();
 
-            var output = OpenApiContractGenerator.Generate(
-                stream,
-                nameSpace,
-                e => context.LogInfo(e)
-            );
-            context.AddSource(nameSpace + ".g.cs", output);
-        }
-        catch (Exception ex)
-        {
-            context.LogError("Error generating remote HTTP contract: " + ex);
-        }
+        var output = OpenApiContractGenerator.Generate(
+            stream,
+            nameSpace,
+            e => context.LogInfo(e)
+        );
+        context.AddSource(nameSpace + ".g.cs", output);
     }
 }
