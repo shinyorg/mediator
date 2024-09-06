ab17ab608dec15a7c7a76ab61ce9be96738e5a27
Allan Ritchie <allan.ritchie@gmail.com>
2024-09-06 12:25:42 -0400

    Trying to pack

---
+++ tests/Shiny.Mediator.Tests/HttpRequestTests.cs
@@ -0,0 +1,20 @@
+using Shiny.Mediator.Http;
+
+namespace Shiny.Mediator.Tests;
+
+public class HttpRequestTests
+{
+        
+}
+
+public class MyHttpResultRequest : IHttpRequest<HttpResult>
+{
+    
+}
+
+// TODO: void, easy response
+
+public class HttpResult
+{
+    
+}
