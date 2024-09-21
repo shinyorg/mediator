360188741b5876ef717d76379de6f311d188368f
Allan Ritchie <allan.ritchie@gmail.com>
2024-09-21 13:41:59 -0400

    Offline ready for config tests

--- src/Shiny.Mediator.AppSupport/Middleware/OfflineAvailableRequestMiddleware.cs
+++ src/Shiny.Mediator.AppSupport/Middleware/OfflineAvailableRequestMiddleware.cs
@@ -8,7 +8,7 @@ namespace Shiny.Mediator.Middleware;
 public class OfflineAvailableRequestMiddleware<TRequest, TResult>(
     IInternetService connectivity, 
     IStorageService storage,
-    IConfiguration config
+    IConfiguration configuration
 ) : IRequestMiddleware<TRequest, TResult>
 {
     public async Task<TResult> Process(
@@ -21,21 +21,32 @@ public class OfflineAvailableRequestMiddleware<TRequest, TResult>(
         if (typeof(TResult) == typeof(Unit))
             return await next().ConfigureAwait(false);
 
-        var cfg = requestHandler.GetHandlerHandleMethodAttribute<TRequest, OfflineAvailableAttribute>();
-        cfg ??= request!.GetType().GetCustomAttribute<OfflineAvailableAttribute>();
-        if (cfg == null)
-            return await next().ConfigureAwait(false);
-        
+        var acrossSessions = true;
+        var section = configuration.GetHandlerSection("Offline", request!, this);
+        if (section == null)
+        {
+            var cfg = requestHandler.GetHandlerHandleMethodAttribute<TRequest, OfflineAvailableAttribute>();
+            cfg ??= request!.GetType().GetCustomAttribute<OfflineAvailableAttribute>();
+            if (cfg == null)
+                return await next().ConfigureAwait(false);
+            
+            acrossSessions = cfg.AvailableAcrossSessions;
+        }
+        else
+        {
+            acrossSessions = section.GetValue("AvailableAcrossSessions", acrossSessions);
+        }
+
         var result = default(TResult);
         if (connectivity.IsAvailable)
         {
             result = await next().ConfigureAwait(false);
             if (result != null)
-                await storage.Store(request!, result, cfg.AvailableAcrossSessions);
+                await storage.Store(request!, result, acrossSessions);
         }
         else
         {
-            result = await storage.Get<TResult>(request, cfg.AvailableAcrossSessions);
+            result = await storage.Get<TResult>(request!, acrossSessions);
         }
         return result;
     }
