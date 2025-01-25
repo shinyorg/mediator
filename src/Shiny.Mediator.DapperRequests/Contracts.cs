816d30b64fcb7c2626b405ce347858cf8baa26f3
Allan Ritchie <allan.ritchie@gmail.com>
2025-01-24 21:42:59 -0500

    Dapper request key

---
+++ src/Shiny.Mediator.DapperRequests/Contracts.cs
@@ -0,0 +1,47 @@
+﻿namespace Shiny.Mediator;
+
+
+public record DapperRequest<TResult>(
+    FormattableString Sql, 
+    int? CommandTimeout = null,
+    string? RequestKey = null
+) : IRequest<TResult>, IRequestKey 
+{
+    public string GetKey()
+    {
+        if (this.RequestKey != null)
+            return this.RequestKey;
+
+        var key = typeof(TResult).FullName + "_" + Sql;
+        return key;
+    }
+}
+public record DapperQuery<TResult>(
+    FormattableString Sql, 
+    int? CommandTimeout = null,
+    string? RequestKey = null
+) : DapperRequest<IEnumerable<TResult>>(
+    Sql,
+    CommandTimeout,
+    RequestKey
+);
+
+public record DapperFirstQuery<TResult>(
+    FormattableString Sql, 
+    int? CommandTimeout = null,
+    string? RequestKey = null
+) : DapperRequest<TResult>(
+    Sql,
+    CommandTimeout,
+    RequestKey
+);
+
+public record DapperScalar(
+    FormattableString Sql, 
+    int? CommandTimeout = null,
+    string? RequestKey = null
+) : DapperRequest<object>(
+    Sql,
+    CommandTimeout,
+    RequestKey
+);
