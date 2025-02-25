ac8a0301d27c964685787ea80512566857a1da87
Allan Ritchie <allan.ritchie@gmail.com>
2025-02-24 21:50:50 -0500

    Missed updates

--- src/Shiny.Mediator.Sentry/Infrastructure/SentryExceptionHandler.cs
+++ src/Shiny.Mediator.Sentry/Infrastructure/SentryExceptionHandler.cs
@@ -2,7 +2,7 @@ namespace Shiny.Mediator.Infrastructure;
 
 public class SentryExceptionHandler : IExceptionHandler
 {
-    public async Task<bool> Handle(object message, object handler, Exception exception)
+    public async Task<bool> Handle(object message, object handler, Exception exception, IMediatorContext context)
     {
         SentrySdk.CaptureException(exception);
         return false;
