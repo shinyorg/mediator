252fed190a7bc9a6b4661dd5302a0e7bfa2ab614
Allan Ritchie <allan.ritchie@gmail.com>
2025-03-04 17:42:19 -0500

    Update FlushStoreEvents.cs

--- src/Shiny.Mediator/FlushStoreEvents.cs
+++ src/Shiny.Mediator/FlushStoreEvents.cs
@@ -14,6 +14,14 @@ public static class FlushStoreExtensions
     public static Task FlushAllStores(this IMediator mediator, CancellationToken cancellationToken = default)
         => mediator.Publish(new FlushAllStoresEvent(), cancellationToken);
     
+    /// <summary>
+    /// All middleware within Shiny will respond to this event
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public static Task FlushAllStores(this IMediatorContext context, CancellationToken cancellationToken = default)
+        => context.Publish(new FlushAllStoresEvent(), true, cancellationToken);
     
     /// <summary>
     /// Flushes store by type and/or keys starting with prefix
@@ -25,4 +33,16 @@ public static class FlushStoreExtensions
     /// <returns></returns>
     public static Task FlushStores(this IMediator mediator, string requestKey, bool partialMatch = false, CancellationToken cancellationToken = default)
         => mediator.Publish(new FlushStoresEvent(requestKey, partialMatch), cancellationToken);
+    
+    
+    /// <summary>
+    /// Flushes store by type and/or keys starting with prefix
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="requestKey"></param>
+    /// <param name="partialMatch"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public static Task FlushStores(this IMediatorContext context, string requestKey, bool partialMatch = false, CancellationToken cancellationToken = default)
+        => context.Publish(new FlushStoresEvent(requestKey, partialMatch), true, cancellationToken);
 }
