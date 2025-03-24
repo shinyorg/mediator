86bc02fd9ac47c6b1e2828d4738d9d1cbac60534
Allan Ritchie <allan.ritchie@gmail.com>
2025-03-24 10:34:24 -0400

    Add middleware tests

--- tests/Shiny.Mediator.Tests/Caching/CachingRequestMiddlewareTests.cs
+++ tests/Shiny.Mediator.Tests/Caching/CachingRequestMiddlewareTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Time.Testing;
+using Shiny.Mediator.Caching;
 using Shiny.Mediator.Caching.Infrastructure;
 using Shiny.Mediator.Tests.Mocks;
 using Xunit.Abstractions;
@@ -29,10 +30,50 @@ public class CachingRequestMiddlewareTests
     
     
     [Fact]
-    public void Test()
+    public async Task EndToEnd_NotFromCache()
     {
-
+        var handler = new RequestHandlerDelegate<string>(() => Task.FromResult("NotFromCache"));
+        var context = new MockMediatorContext
+        {
+            Message = new CachingRequestMiddlewareRequest("Hello"),
+            MessageHandler = new CacheRequestMiddlewareTestHandler()
+        };
+        var result = await this.middleware.Process(context, handler, CancellationToken.None);
+        
+        result.ShouldBe("NotFromCache");
+    }
+    
+    
+    [Fact]
+    public async Task EndToEnd_FromCache()
+    {
+        await this.cache.Set("Test", "FromCache");
+        
+        var handler = new RequestHandlerDelegate<string>(() => Task.FromResult("NotFromCache"));
+        var context = new MockMediatorContext
+        {
+            Message = new CachingRequestMiddlewareRequest("Hello"),
+            MessageHandler = new CacheRequestMiddlewareTestHandler()
+        };
+        var result = await this.middleware.Process(context, handler, CancellationToken.None);
+        
+        result.ShouldBe("FromCache");
     }
 }
 
-public record CachingRequestMiddlewareRequest(string Value) : IRequest<string>;
+public record CachingRequestMiddlewareRequest(string Value) : IRequest<string>, ICacheControl, IRequestKey
+{
+    public bool ForceRefresh { get; set; }
+    public TimeSpan? AbsoluteExpiration { get; set; }
+    public TimeSpan? SlidingExpiration { get; set; }
+
+    public string GetKey() => "Test";
+};
+
+public class CacheRequestMiddlewareTestHandler : IRequestHandler<CachingRequestMiddlewareRequest, string>
+{
+    public Task<string> Handle(CachingRequestMiddlewareRequest request, IMediatorContext context, CancellationToken cancellationToken)
+    {
+        return Task.FromResult("FromHandler");
+    }
+}
