051203d65fbf263263ecfaef698d958da7d5ae22
Allan Ritchie <allan.ritchie@gmail.com>
2025-04-24 15:03:22 -0400

    Even if mediator sentry extension is enabled, we now ensure we don't do sentry things if sentry isn't enabled

--- src/Shiny.Mediator.Sentry/Infrastructure/SentryCommandMiddleware.cs
+++ src/Shiny.Mediator.Sentry/Infrastructure/SentryCommandMiddleware.cs
@@ -1,10 +1,15 @@
 namespace Shiny.Mediator.Infrastructure;
 
 
-public class SentryCommandMiddleware<TCommand>(IHub hub) : ICommandMiddleware<TCommand> where TCommand : ICommand
+public class SentryCommandMiddleware<TCommand>(IHub? hub = null) : ICommandMiddleware<TCommand> where TCommand : ICommand
 {
     public async Task Process(IMediatorContext context, CommandHandlerDelegate next, CancellationToken cancellationToken)
     {
+        if (hub == null)
+        {
+            await next().ConfigureAwait(false);
+            return;
+        }
         var transaction = hub.StartTransaction("mediator", "event");
         var span = transaction.StartChild(context.MessageHandler.GetType().FullName!);
         await next().ConfigureAwait(false);
--- src/Shiny.Mediator.Sentry/Infrastructure/SentryExceptionHandler.cs
+++ src/Shiny.Mediator.Sentry/Infrastructure/SentryExceptionHandler.cs
@@ -1,10 +1,10 @@
 namespace Shiny.Mediator.Infrastructure;
 
-public class SentryExceptionHandler(IHub hub) : IExceptionHandler
+public class SentryExceptionHandler(IHub? hub = null) : IExceptionHandler
 {
     public Task<bool> Handle(IMediatorContext context, Exception exception)
     {
-        hub.CaptureException(exception);
+        hub?.CaptureException(exception);
         return Task.FromResult(false);
     }
 }
