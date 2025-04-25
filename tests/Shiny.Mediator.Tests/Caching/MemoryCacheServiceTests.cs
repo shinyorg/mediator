55decf2b153e5fc77097ae3eb583e351a084be38
Allan Ritchie <allan.ritchie@gmail.com>
2025-04-25 10:08:32 -0400

    Fix #30 - MSFT MemoryCache Set was not committing entry

--- tests/Shiny.Mediator.Tests/Caching/MemoryCacheServiceTests.cs
+++ tests/Shiny.Mediator.Tests/Caching/MemoryCacheServiceTests.cs
@@ -1,8 +1,8 @@
-using DryIoc;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Time.Testing;
 
 namespace Shiny.Mediator.Tests.Caching;
+
 
 public class MemoryCacheServiceTests
 {
@@ -21,7 +21,42 @@ public class MemoryCacheServiceTests
         secondResult.ShouldNotBeNull();
         result.Value.ShouldBe(secondResult.Value); // should have retrieved from cache
     }
+    
+    
+    [Fact]
+    public async Task GetSet_SimpleType_Test()
+    {
+        var fake = new FakeTimeProvider();
+        var memCache = new MemoryCache(new MemoryCacheOptions());
+        
+        var service = new MemoryCacheService(memCache, fake);
+        var result = await service.Set("test", fake.GetTimestamp());
+        
+        result.ShouldNotBeNull();
+        var secondResult = await service.Get<long>("test");
+        
+        secondResult.ShouldNotBeNull();
+        result.Value.ShouldBe(secondResult.Value);
+    }
+    
 
+    [Fact]
+    public async Task GetSet_ReferenceType_Test()
+    {
+        var fake = new FakeTimeProvider();
+        var memCache = new MemoryCache(new MemoryCacheOptions());
+        
+        var obj = new MyClass { Id = 123, Name = "Hello World" };
+        var service = new MemoryCacheService(memCache, fake);
+        var result = await service.Set("myobj", obj);
+        result.ShouldNotBeNull();
+        
+        var secondResult = await service.Get<MyClass>("myobj");
+        secondResult.ShouldNotBeNull();
+        result.Value.Id.ShouldBe(secondResult.Value.Id);
+        result.Value.Name.ShouldBe(secondResult.Value.Name);
+    }
+    
 
     [Fact]
     public void MemoryCache_Clear()
@@ -44,4 +79,10 @@ public class MemoryCacheServiceTests
         entries.ShouldNotBeNull();
         entries.Count.ShouldBe(1);
     }
+}
+
+file class MyClass
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
 }
