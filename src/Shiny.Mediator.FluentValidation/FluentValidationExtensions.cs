d7aca33f9e79a458eec4e98659386eb5e73e7da8
Allan Ritchie <allan.ritchie@gmail.com>
2025-06-16 15:51:32 -0400

    Fluent validation v12 plus tests

--- src/Shiny.Mediator.FluentValidation/FluentValidationExtensions.cs
+++ src/Shiny.Mediator.FluentValidation/FluentValidationExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Shiny.Mediator.FluentValidation.Middleware;
+﻿using System.Reflection;
+using FluentValidation;
+using Shiny.Mediator.FluentValidation.Middleware;
 
 namespace Shiny.Mediator;
 
@@ -9,11 +11,18 @@ public static class FluentValidationExtensions
     /// Adds fluent validation to the mediator pipeline
     /// </summary>
     /// <param name="cfg"></param>
+    /// <param name="validatorAssemblies">
+    /// The assemblies to scan for validators - this may break AOT and is here for convenience
+    /// NOTE: validators must exist on the dependency injection container to be picked up by Shiny Mediator
+    /// </param>
     /// <returns></returns>
-    public static ShinyMediatorBuilder AddFluentValidation(this ShinyMediatorBuilder cfg)
+    public static ShinyMediatorBuilder AddFluentValidation(this ShinyMediatorBuilder cfg, params IEnumerable<Assembly> validatorAssemblies)
     {
         cfg.AddOpenRequestMiddleware(typeof(FluentValidationRequestMiddleware<,>));
         cfg.AddOpenCommandMiddleware(typeof(FluentValidationCommandMiddleware<>));
+        if (validatorAssemblies.Any())
+            cfg.Services.AddValidatorsFromAssemblies(validatorAssemblies);
+
         return cfg;
     }
 }
