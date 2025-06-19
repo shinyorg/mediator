e3c80b6f02f427ffb314412333fa6085c1897314
Allan Ritchie <allan.ritchie@gmail.com>
2025-06-19 17:37:16 -0400

    Test Updates

--- tests/Shiny.Mediator.Tests/Http/OpenApiContractGeneratorTests.cs
+++ tests/Shiny.Mediator.Tests/Http/OpenApiContractGeneratorTests.cs
@@ -4,12 +4,22 @@ using Xunit.Abstractions;
 namespace Shiny.Mediator.Tests.Http;
 
 
+/*
+// put/post body are named the same as the overall request object when a pre/post fix is used
+[global::Shiny.Mediator.Http.HttpAttribute(global::Shiny.Mediator.Http.HttpVerb.Post, "/teams")]
+public partial class CreateTeam : global::Shiny.Mediator.Http.IHttpRequest<global::ShinyScoreboard.ValidateResult>
+{
+   [global::Shiny.Mediator.Http.HttpParameter(global::Shiny.Mediator.Http.HttpParameterType.Body)]
+   public global::ShinyScoreboard.CreateTeam Body { get; set; }
+}
+ */
 public class OpenApiContractGeneratorTests(ITestOutputHelper output)
 {
     [Theory]
     [InlineData("./Http/standard.json", "HttpRequest", "TestApi")]
     [InlineData("./Http/enums.json", null, "TestApi")]
-    public Task Tests(string path, string? contractPostfix, string nameSpace)
+    [InlineData("./Http/validateresult.json", null, "TestApi")]
+    public Task Local_Tests(string path, string? contractPostfix, string nameSpace)
     { 
         using var doc = File.OpenRead(path);
         var generator = new OpenApiContractGenerator(
@@ -32,14 +42,16 @@ public class OpenApiContractGeneratorTests(ITestOutputHelper output)
     {
         var http = new HttpClient();
         var stream = await http.GetStreamAsync(uri);
-        var cfg = new MediatorHttpItemConfig
-        {
-            Namespace = nameSpace,
-            ContractPostfix = "HttpRequest"
-        };
-        var generator = new OpenApiContractGenerator(cfg, (msg, level) => output.WriteLine(msg));
-        var code = generator.Generate(stream);
+        var generator = new OpenApiContractGenerator(
+            new MediatorHttpItemConfig
+            {
+                Namespace = nameSpace,
+                ContractPostfix = "HttpRequest"
+            }, 
+            (msg, severity) => output.WriteLine($"[{severity}] {msg}")
+        );
+        var content = generator.Generate(stream);
     
-        output.WriteLine(code);
+        await Verify(content).UseParameters(uri, nameSpace);
     }
 }
