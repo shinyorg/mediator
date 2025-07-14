fcd1df0feab35f67d287c74f84d6fdade35ec8eb
Allan Ritchie <allan.ritchie@gmail.com>
2025-07-14 17:10:50 -0400

    Update Program.cs

--- samples/Sample.Blazor/Program.cs
+++ samples/Sample.Blazor/Program.cs
@@ -13,11 +13,9 @@ builder.Configuration.AddInMemoryCollection(new Dictionary<string, string>
     { "Mediator:UserErrorNotifications:Sample.Blazor.Handlers.SafeErrorCommand:fr-CA:Message", "UserErrorNotification is emitting this message for French Users" },
 }!);
 
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
-builder.Services.AddShinyMediator(x =>
-{
-    x.UseBlazor();
-});
+builder.Services.AddShinyMediator(x => x.UseBlazor());
 builder.Services.AddDiscoveredMediatorHandlersFromSample_Blazor();
 
 await builder.Build().RunAsync();
