8402117c5b10eb14f9de7909028bd80aa75a152e
Allan Ritchie <allan.ritchie@gmail.com>
2025-07-14 16:56:13 -0400

    Improved blazor sample

--- samples/Sample.Blazor/Handlers/ErrorCommandHandler.cs
+++ samples/Sample.Blazor/Handlers/ErrorCommandHandler.cs
@@ -2,14 +2,21 @@ using Shiny.Mediator;
 
 namespace Sample.Blazor.Handlers;
 
-public record ErrorCommand(bool HandleIt) : ICommand;
+public record ErrorCommand : ICommand;
+
+public record SafeErrorCommand : ICommand;
 
 
 [SingletonHandler]
-public class ErrorCommandHandler : ICommandHandler<ErrorCommand>
+public class ErrorCommandHandler : ICommandHandler<ErrorCommand>, ICommandHandler<SafeErrorCommand>
 {
     public Task Handle(ErrorCommand command, IMediatorContext context, CancellationToken cancellationToken)
     {
         throw new InvalidOperationException("Handler BOOOM!");
+    }
+
+    public Task Handle(SafeErrorCommand command, IMediatorContext context, CancellationToken cancellationToken)
+    {
+        throw new InvalidOperationException("I also blow up, but you should see an alert instead of Blazor crashing");
     }
 }
--- samples/Sample.Blazor/Program.cs
+++ samples/Sample.Blazor/Program.cs
@@ -1,17 +1,22 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Sample.Blazor;
-using Sample.Blazor.Handlers;
 using Shiny.Mediator;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Configuration.AddInMemoryCollection(new Dictionary<string, string>
+{
+    { "Mediator:UserErrorNotifications:Sample.Blazor.Handlers.SafeErrorCommand:*:Message", "UserErrorNotification is emitting this message" },
+    { "Mediator:UserErrorNotifications:Sample.Blazor.Handlers.SafeErrorCommand:fr-CA:Message", "UserErrorNotification is emitting this message for French Users" },
+}!);
+
+
 builder.Services.AddShinyMediator(x =>
 {
     x.UseBlazor();
-    x.AddExceptionHandler<TestExceptionHandler>();
 });
 builder.Services.AddDiscoveredMediatorHandlersFromSample_Blazor();
 
