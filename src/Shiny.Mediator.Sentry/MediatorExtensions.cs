944cd6cfb7b5c7126b148e7160f65a8c3a382bc2
Allan Ritchie <allan.ritchie@gmail.com>
2025-07-29 11:17:42 -0400

    Reintroduce sentry since their diagnostics package won't AOT

---
+++ src/Shiny.Mediator.Sentry/MediatorExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Shiny.Mediator;
+
+
+public static class MediatorExtensions
+{
+    /// <summary>
+    /// Wires up Sentry to Mediator middleware - be sure to call UseSentry on your hosting provider
+    /// </summary>
+    /// <param name="configurator"></param>
+    /// <returns></returns>
+    public static ShinyMediatorBuilder UseSentry(this ShinyMediatorBuilder configurator)
+    {
+        // configurator.AddExceptionHandler<SentryExceptionHandler>();
+        // configurator.AddOpenEventMiddleware(typeof(SentryEventMiddleware<>), ServiceLifetime.Singleton);
+        // configurator.AddOpenCommandMiddleware(typeof(SentryCommandMiddleware<>), ServiceLifetime.Singleton);
+        // configurator.AddOpenRequestMiddleware(typeof(SentryRequestMiddleware<,>), ServiceLifetime.Singleton);
+        // configurator.AddOpenStreamMiddleware(typeof(SentryStreamRequestMiddleware<,>), ServiceLifetime.Singleton);
+        return configurator;
+    }  
+}
