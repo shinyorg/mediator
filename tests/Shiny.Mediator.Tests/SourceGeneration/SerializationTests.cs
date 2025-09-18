9cfa3718c0ad6db109c7ffd6dcd5202328570782
Allan Ritchie <allan.ritchie@gmail.com>
2025-09-18 19:53:43 -0400

    Test fixes

--- tests/Shiny.Mediator.Tests/SourceGeneration/SerializationTests.cs
+++ tests/Shiny.Mediator.Tests/SourceGeneration/SerializationTests.cs
@@ -3,53 +3,84 @@ using System.Text.Json;
 namespace Shiny.Mediator.Tests.SourceGeneration;
 
 
-[SourceGenerateJsonConverter]
-public partial class MyTestClass
-{
-    public Guid? GuidValue { get; set; }
-    public string StringValue { get; set; }
-    
-    public int? IntValue { get; set; }
-    
-    public DateTimeOffset? DateValue { get; set; }
-    
-    public List<MyTestClass> Children { get; set; } = new();
-}
-
 public class SerializationTests
 {
+
     [Fact]
-    public Task Serialize()
+    public Task Reader()
     {
-        var obj = new MyTestClass
-        {
-            GuidValue = new Guid("5ae5016e-d35b-4846-91f2-2ff1cace4b97"),
-            StringValue = "Test",
-            DateValue = new DateTime(2023, 1, 1, 12, 0, 0, DateTimeKind.Utc),
-            Children = [
-                new()
-                {
-                    IntValue = 2
-                }
-            ]
-        };
+        var json = File.ReadAllText("./SourceGeneration/serialization.json");
+        var obj = JsonSerializer.Deserialize<EntityLiveDataResponse>(json);
+        return Verify(obj);
+    }
+
+
+    [Fact]
+    public Task Writer()
+    {
+        var json = File.ReadAllText("./SourceGeneration/serialization.json");
+        var obj = JsonSerializer.Deserialize<EntityLiveDataResponse>(json);
         
-        var json = JsonSerializer.Serialize(obj);
-
-        return Verify(json).DontScrubDateTimes();
+        var serializedJson = JsonSerializer.Serialize(obj);
+        return Verify(serializedJson);
     }
+}
 
+[SourceGenerateJsonConverter]
+public partial class EntityLiveDataResponse
+{
+    [global::System.Text.Json.Serialization.JsonPropertyName("id")]
+    public string Id { get; set; }
 
-    [Fact]
-    public Task Deserialize()
-    {
-        var obj = JsonSerializer.Deserialize<MyTestClass>(
-            """
-            {
-                 "DateValue": "2015-11-19T00:00:00"
-            }
-            """);
+    [global::System.Text.Json.Serialization.JsonPropertyName("name")]
+    public string Name { get; set; }
 
-        return Verify(obj).DontScrubDateTimes();
-    }
+    [global::System.Text.Json.Serialization.JsonPropertyName("entityType")]
+    public EntityType EntityType { get; set; }
+
+    [global::System.Text.Json.Serialization.JsonPropertyName("timezone")]
+    public string Timezone { get; set; }
+
+    [global::System.Text.Json.Serialization.JsonPropertyName("liveData")]
+    public List<EntityLiveData> LiveData { get; set; }
+
+}
+
+[SourceGenerateJsonConverter]
+public partial class EntityLiveData
+{
+    [global::System.Text.Json.Serialization.JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [global::System.Text.Json.Serialization.JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [global::System.Text.Json.Serialization.JsonPropertyName("entityType")]
+    public EntityType EntityType { get; set; }
+
+    // [global::System.Text.Json.Serialization.JsonPropertyName("lastUpdated")]
+    // public System.DateTimeOffset LastUpdated { get; set; }
+
+    // [global::System.Text.Json.Serialization.JsonPropertyName("queue")]
+    // public global::ShinyWonderland.ThemeParksApi.LiveQueue Queue { get; set; }
+    //
+    // [global::System.Text.Json.Serialization.JsonPropertyName("showtimes")]
+    // public global::System.Collections.Generic.List<global::ShinyWonderland.ThemeParksApi.LiveShowTime> Showtimes { get; set; }
+    //
+    // [global::System.Text.Json.Serialization.JsonPropertyName("operatingHours")]
+    // public global::System.Collections.Generic.List<global::ShinyWonderland.ThemeParksApi.LiveShowTime> OperatingHours { get; set; }
+    //
+    // [global::System.Text.Json.Serialization.JsonPropertyName("diningAvailability")]
+    // public global::System.Collections.Generic.List<global::ShinyWonderland.ThemeParksApi.DiningAvailability> DiningAvailability { get; set; }
+
+}
+
+public enum EntityType
+{
+    DESTINATION,
+    PARK,
+    ATTRACTION,
+    RESTAURANT,
+    HOTEL,
+    SHOW,
 }
