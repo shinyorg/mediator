08863e4cd8e20986836f8cceae140a3943ec7498
Allan Ritchie <allan.ritchie@gmail.com>
2025-10-26 22:47:34 -0400

    Updates

--- samples/Sample.Api/Program.cs
+++ samples/Sample.Api/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
 using Sample.Api;
+using Sample.Api.Handlers;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,5 +16,29 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 app.MapGeneratedMediatorEndpoints();
+app.MapStaticAssets();
+
+// app.MapGet(
+//     "/sse",
+//     async (
+//         [FromServices] IMediator mediator,
+//         [FromServices] IHttpContextAccessor context
+//     ) =>
+//     {
+//         context.HttpContext.Response.Headers.ContentType = "text/event-stream";
+//         context.HttpContext.Response.Headers.CacheControl = "no-cache";
+//         context.HttpContext.Response.Headers.Connection = "keep-alive";
+//
+//         // while (!context.HttpContext.RequestAborted.IsCancellationRequested)
+//         // {
+//             var stream = mediator.Request(new TickerStreamRequest(999, 1000));
+//             await foreach (var ticker in stream)
+//             {
+//                 await context.HttpContext.Response.WriteAsync(ticker.Result.ToString());
+//                 await context.HttpContext.Response.Body.FlushAsync();
+//             }
+//         // }
+//     }
+// );
 
 app.Run();
--- src/Shiny.Mediator/MediatorExtensions.cs
+++ src/Shiny.Mediator/MediatorExtensions.cs
@@ -9,11 +9,13 @@ public static class MediatorExtensions
     /// Wait for event handler to fire
     /// </summary>
     /// <param name="mediator"></param>
+    /// <param name="filter">Allows you to filter the event instead of completing</param>
     /// <param name="cancellationToken"></param>
     /// <typeparam name="T"></typeparam>
     /// <returns></returns>
     public static async Task<T> WaitForEvent<T>(
         this IMediator mediator, 
+        Func<T, bool>? filter = null,
         CancellationToken cancellationToken = default
     ) where T : IEvent
     {
@@ -21,7 +23,8 @@ public static class MediatorExtensions
         await using var u1 = cancellationToken.Register(() => tcs.TrySetCanceled(cancellationToken));
         using var u2 = mediator.Subscribe<T>((ev, ctx, ct) =>
         {
-            tcs.TrySetResult(ev);
+            if (filter?.Invoke(ev) ?? true)
+                tcs.TrySetResult(ev);
             return Task.CompletedTask;
         });
 
@@ -33,17 +36,19 @@ public static class MediatorExtensions
     /// Continues to wait for event handler responses
     /// </summary>
     /// <param name="mediator"></param>
+    /// <param name="filter">Allows you to filter the event before streaming it</param>
     /// <param name="cancellationToken"></param>
     /// <typeparam name="T"></typeparam>
     /// <returns></returns>
     public static async IAsyncEnumerable<T> EventStream<T>(
         this IMediator mediator, 
+        Func<T, bool>? filter = null,
         [EnumeratorCancellation] CancellationToken cancellationToken = default
     ) where T : IEvent
     {
         while (!cancellationToken.IsCancellationRequested)
         {
-            var e = await mediator.WaitForEvent<T>(cancellationToken);
+            var e = await mediator.WaitForEvent<T>(filter, cancellationToken);
             yield return e;
         }
     }
