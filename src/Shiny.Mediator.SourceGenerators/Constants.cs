7c1fd099d19754f23669bbd8f8620470d8e83820
Allan Ritchie <allan.ritchie@gmail.com>
2025-11-19 11:27:41 -0500

    Update source gen version

--- src/Shiny.Mediator.SourceGenerators/Constants.cs
+++ src/Shiny.Mediator.SourceGenerators/Constants.cs
@@ -2,5 +2,5 @@ namespace Shiny.Mediator.SourceGenerators;
 
 public static class Constants
 {
-    public const string GeneratedCodeAttributeString = "[global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"Shiny.Mediator.SourceGenerators\", \"5.0.0\")]";
+    public const string GeneratedCodeAttributeString = "[global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"Shiny.Mediator.SourceGenerators\", \"6.0.0\")]";
 }
