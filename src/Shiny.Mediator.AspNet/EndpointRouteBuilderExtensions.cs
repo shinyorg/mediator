497458bcc7a08e0352c7829ad64f56874b983d57
Allan Ritchie <allan.ritchie@gmail.com>
2025-11-19 11:51:00 -0500

    Add streaming extension

--- src/Shiny.Mediator.AspNet/EndpointRouteBuilderExtensions.cs
+++ src/Shiny.Mediator.AspNet/EndpointRouteBuilderExtensions.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -8,163 +9,209 @@ namespace Shiny.Mediator;
 
 public static class EndpointRouteBuilderExtensions
 {
-    // TODO: streaming to signalr
-    
     #region Requests
     
-    public static RouteHandlerBuilder MapMediatorGet<TRequest, TResult>(this IEndpointRouteBuilder builder, string pattern)
-        where TRequest : IRequest<TResult>
-    => builder.MapGet(
-        pattern, 
-        async (
-            [FromServices] IMediator mediator,
-            [AsParameters] TRequest request,
-            CancellationToken cancellationToken
-        ) =>
-        {
-            var result = await mediator
-                .Request(request, cancellationToken)
-                .ConfigureAwait(false);
+    extension(IEndpointRouteBuilder builder)
+    {
+        public RouteHandlerBuilder MapMediatorGet<TRequest, TResult>(string pattern) where TRequest : IRequest<TResult>
+            => builder.MapGet(
+                pattern, 
+                async (
+                    [FromServices] IMediator mediator,
+                    [AsParameters] TRequest request,
+                    CancellationToken cancellationToken
+                ) =>
+                {
+                    var result = await mediator
+                        .Request(request, cancellationToken)
+                        .ConfigureAwait(false);
 
-            return TypedResults.Ok(result.Result);
-        }
-    );
-    
-    
-    public static RouteHandlerBuilder MapMediatorPost<TRequest, TResult>(this IEndpointRouteBuilder builder, string pattern)
-        where TRequest : IRequest<TResult>
-    => builder.MapPost(
-        pattern, 
-        async (
-            [FromServices] IMediator mediator,
-            [FromBody] TRequest request,
-            CancellationToken cancellationToken
-        ) =>
-        {
-            var result = await mediator
-                .Request(request, cancellationToken)
-                .ConfigureAwait(false);
+                    return TypedResults.Ok(result.Result);
+                }
+            );
 
-            return TypedResults.Ok(result.Result);
-        }
-    );
-    
-    
-    public static RouteHandlerBuilder MapMediatorPut<TRequest, TResult>(this IEndpointRouteBuilder builder, string pattern)
-        where TRequest : IRequest<TResult>
-    => builder.MapPut(
-        pattern, 
-        async (
-            [FromServices] IMediator mediator,
-            [FromBody] TRequest request,
-            CancellationToken cancellationToken
-        ) =>
-        {
-            var result = await mediator
-                .Request(request, cancellationToken)
-                .ConfigureAwait(false);
 
-            return TypedResults.Ok(result.Result);
-        }
-    ); 
-    
-    
-    public static RouteHandlerBuilder MapMediatorDelete<TRequest, TResult>(this IEndpointRouteBuilder builder, string pattern)
-        where TRequest : IRequest<TResult>
-    => builder.MapDelete(
-        pattern, 
-        async (
-            [FromServices] IMediator mediator,
-            [AsParameters] TRequest request,
-            CancellationToken cancellationToken
-        ) =>
-        {
-            var result = await mediator
-                .Request(request, cancellationToken)
-                .ConfigureAwait(false);
+        public RouteHandlerBuilder MapMediatorPost<TRequest, TResult>(string pattern) where TRequest : IRequest<TResult>
+            => builder.MapPost(
+                pattern, 
+                async (
+                    [FromServices] IMediator mediator,
+                    [FromBody] TRequest request,
+                    CancellationToken cancellationToken
+                ) =>
+                {
+                    var result = await mediator
+                        .Request(request, cancellationToken)
+                        .ConfigureAwait(false);
 
-            return TypedResults.Ok(result.Result);
-        }
-    );
-    
+                    return TypedResults.Ok(result.Result);
+                }
+            );
+
+
+        public RouteHandlerBuilder MapMediatorPut<TRequest, TResult>(string pattern) where TRequest : IRequest<TResult>
+            => builder.MapPut(
+                pattern, 
+                async (
+                    [FromServices] IMediator mediator,
+                    [FromBody] TRequest request,
+                    CancellationToken cancellationToken
+                ) =>
+                {
+                    var result = await mediator
+                        .Request(request, cancellationToken)
+                        .ConfigureAwait(false);
+
+                    return TypedResults.Ok(result.Result);
+                }
+            );
+
+
+        public RouteHandlerBuilder MapMediatorDelete<TRequest, TResult>(string pattern) where TRequest : IRequest<TResult>
+            => builder.MapDelete(
+                pattern, 
+                async (
+                    [FromServices] IMediator mediator,
+                    [AsParameters] TRequest request,
+                    CancellationToken cancellationToken
+                ) =>
+                {
+                    var result = await mediator
+                        .Request(request, cancellationToken)
+                        .ConfigureAwait(false);
+
+                    return TypedResults.Ok(result.Result);
+                }
+            );
+    }
+
     #endregion
     
     #region Commands
 
-    public static RouteHandlerBuilder MapMediatorGet<TCommand>(this IEndpointRouteBuilder builder, string pattern)
-        where TCommand : ICommand
-    => builder.MapGet(
-        pattern, 
-        async (
-            [FromServices] IMediator mediator,
-            [AsParameters] TCommand command,
-            CancellationToken cancellationToken
-        ) =>
-        {
-            await mediator
-                .Send(command, cancellationToken)
-                .ConfigureAwait(false);
+    extension(IEndpointRouteBuilder builder)
+    {
+        public RouteHandlerBuilder MapMediatorGet<TCommand>(string pattern) where TCommand : ICommand
+            => builder.MapGet(
+                pattern, 
+                async (
+                    [FromServices] IMediator mediator,
+                    [AsParameters] TCommand command,
+                    CancellationToken cancellationToken
+                ) =>
+                {
+                    await mediator
+                        .Send(command, cancellationToken)
+                        .ConfigureAwait(false);
 
-            return Results.Ok();
-        }
-    );
-    
-    
-    public static RouteHandlerBuilder MapMediatorDelete<TCommand>(this IEndpointRouteBuilder builder, string pattern)
-        where TCommand : ICommand
-    => builder.MapDelete(
-        pattern, 
-        async (
-            [FromServices] IMediator mediator,
-            [AsParameters] TCommand command,
-            CancellationToken cancellationToken
-        ) =>
-        {
-            await mediator
-                .Send(command, cancellationToken)
-                .ConfigureAwait(false);
+                    return Results.Ok();
+                }
+            );
 
-            return Results.Ok();
-        }
-    );
-    
-    
-    public static RouteHandlerBuilder MapMediatorPut<TCommand>(this IEndpointRouteBuilder builder, string pattern)
-        where TCommand : ICommand
-    => builder.MapPut(
-        pattern, 
-        async (
-            [FromServices] IMediator mediator,
-            [FromBody] TCommand command,
-            CancellationToken cancellationToken
-        ) =>
-        {
-            await mediator
-                .Send(command, cancellationToken)
-                .ConfigureAwait(false);
 
-            return Results.Ok();
-        }
-    );
-    
-    
-    public static RouteHandlerBuilder MapMediatorPost<TCommand>(this IEndpointRouteBuilder builder, string pattern)
-        where TCommand : ICommand
-    => builder.MapPost(
-        pattern, 
-        async (
-            [FromServices] IMediator mediator,
-            [FromBody] TCommand command,
-            CancellationToken cancellationToken
-        ) =>
-        {
-            await mediator
-                .Send(command, cancellationToken)
-                .ConfigureAwait(false);
+        public RouteHandlerBuilder MapMediatorDelete<TCommand>(string pattern) where TCommand : ICommand
+            => builder.MapDelete(
+                pattern, 
+                async (
+                    [FromServices] IMediator mediator,
+                    [AsParameters] TCommand command,
+                    CancellationToken cancellationToken
+                ) =>
+                {
+                    await mediator
+                        .Send(command, cancellationToken)
+                        .ConfigureAwait(false);
 
-            return Results.Ok();
-        }
-    );
+                    return Results.Ok();
+                }
+            );
+
+
+        public RouteHandlerBuilder MapMediatorPut<TCommand>(string pattern) where TCommand : ICommand
+            => builder.MapPut(
+                pattern, 
+                async (
+                    [FromServices] IMediator mediator,
+                    [FromBody] TCommand command,
+                    CancellationToken cancellationToken
+                ) =>
+                {
+                    await mediator
+                        .Send(command, cancellationToken)
+                        .ConfigureAwait(false);
+
+                    return Results.Ok();
+                }
+            );
+
+
+        public RouteHandlerBuilder MapMediatorPost<TCommand>(string pattern) where TCommand : ICommand
+            => builder.MapPost(
+                pattern, 
+                async (
+                    [FromServices] IMediator mediator,
+                    [FromBody] TCommand command,
+                    CancellationToken cancellationToken
+                ) =>
+                {
+                    await mediator
+                        .Send(command, cancellationToken)
+                        .ConfigureAwait(false);
+
+                    return Results.Ok();
+                }
+            );
+    }
+
+    #endregion
+    
+    #region Stream Requests
+    
+    extension(IEndpointRouteBuilder builder)
+    {
+        public RouteHandlerBuilder MapMediatorStreamGet<TRequest, TResult>(string pattern) where TRequest : IStreamRequest<TResult>
+            => builder.MapGet(
+                pattern, 
+                (
+                    [FromServices] IMediator mediator,
+                    [AsParameters] TRequest request,
+                    CancellationToken cancellationToken
+                ) =>
+                {
+                    var result = mediator
+                        .Request(request, cancellationToken)
+                        .ConfigureAwait(false);
+
+                    return TypedResults.Ok(UnwrapMediatorAsyncEnumerable(result));
+                }
+            );
+
+
+        public RouteHandlerBuilder MapMediatorStreamPost<TRequest, TResult>(string pattern)
+            where TRequest : IStreamRequest<TResult>
+            => builder.MapPost(
+                pattern, 
+                (
+                    [FromServices] IMediator mediator,
+                    [FromBody] TRequest request,
+                    CancellationToken cancellationToken
+                ) =>
+                {
+                    var result = mediator
+                        .Request(request, cancellationToken)
+                        .ConfigureAwait(false);
+
+                    return TypedResults.Ok(UnwrapMediatorAsyncEnumerable(result));
+                }
+            );
+    }
+
+
+    static async IAsyncEnumerable<T> UnwrapMediatorAsyncEnumerable<T>(ConfiguredCancelableAsyncEnumerable<(IMediatorContext Context, T Result)> source)
+    {
+        await foreach (var item in source)
+            yield return item.Result;
+    }
     
     #endregion
 }
