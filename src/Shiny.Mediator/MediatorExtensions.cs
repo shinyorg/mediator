c59cfaeeba357ba3100cbfd4cde21219921d17b4
Allan Ritchie <allan.ritchie@gmail.com>
2026-01-09 09:47:03 -0500

    Refactor MediatorExtensions event waiting methods
    Replaces WaitForEvent with WaitForSingleEvent and updates EventStream to use Channel for improved event streaming. Adds better cancellation handling and prepares for future mediator context support.

--- src/Shiny.Mediator/MediatorExtensions.cs
+++ src/Shiny.Mediator/MediatorExtensions.cs
@@ -1,59 +1,100 @@
 using System.Runtime.CompilerServices;
+using System.Threading.Channels;
 
 namespace Shiny.Mediator;
 
 
 public static class MediatorExtensions
 {
-    /// <summary>
-    /// Wait for event handler to fire
-    /// </summary>
     /// <param name="mediator"></param>
-    /// <param name="filter">Allows you to filter the event instead of completing</param>
-    /// <param name="cancellationToken"></param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
-    public static async Task<T> WaitForEvent<T>(
-        this IMediator mediator, 
-        Func<T, bool>? filter = null,
-        CancellationToken cancellationToken = default
-    ) where T : IEvent
+    extension(IMediator mediator)
     {
-        var tcs = new TaskCompletionSource<T>();
-        await using var u1 = cancellationToken.Register(() => tcs.TrySetCanceled(cancellationToken));
-        using var u2 = mediator.Subscribe<T>((ev, ctx, ct) =>
+        /// <summary>
+        /// Wait for event handler to fire
+        /// </summary>
+        /// <param name="filter">Allows you to filter the event instead of completing</param>
+        /// <param name="cancellationToken"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public async Task<T> WaitForSingleEvent<T>(Func<T, bool>? filter = null,
+            CancellationToken cancellationToken = default
+        ) where T : IEvent
         {
-            if (filter?.Invoke(ev) ?? true)
-                tcs.TrySetResult(ev);
-            return Task.CompletedTask;
-        });
+            var tcs = new TaskCompletionSource<T>();
+            await using var u1 = cancellationToken.Register(() => tcs.TrySetCanceled(cancellationToken));
+            using var u2 = mediator.Subscribe<T>((ev, ctx, ct) =>
+            {
+                if (filter?.Invoke(ev) ?? true)
+                    tcs.TrySetResult(ev);
+                return Task.CompletedTask;
+            });
 
-        return await tcs.Task.ConfigureAwait(false);
-    }
+            return await tcs.Task.ConfigureAwait(false);
+        }
 
-
-    /// <summary>
-    /// Continues to wait for event handler responses
-    /// </summary>
-    /// <param name="mediator"></param>
-    /// <param name="filter">Allows you to filter the event before streaming it</param>
-    /// <param name="cancellationToken"></param>
-    /// <typeparam name="T"></typeparam>
-    /// <returns></returns>
-    public static async IAsyncEnumerable<T> EventStream<T>(
-        this IMediator mediator, 
-        Func<T, bool>? filter = null,
-        [EnumeratorCancellation] CancellationToken cancellationToken = default
-    ) where T : IEvent
-    {
-        while (!cancellationToken.IsCancellationRequested)
+        /// <summary>
+        /// Continues to wait for event handler responses
+        /// </summary>
+        /// <param name="filter">Allows you to filter the event before streaming it</param>
+        /// <param name="cancellationToken"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public async IAsyncEnumerable<T> EventStream<T>(Func<T, bool>? filter = null,
+            [EnumeratorCancellation] CancellationToken cancellationToken = default
+        ) where T : IEvent
         {
-            var e = await mediator.WaitForEvent<T>(filter, cancellationToken);
-            yield return e;
+            var channel = Channel.CreateUnbounded<T>(new UnboundedChannelOptions
+            {
+                SingleReader = false,
+                SingleWriter = false
+            });
+        
+            var tcs = new TaskCompletionSource<T>();
+            await using var u1 = cancellationToken.Register(() => tcs.TrySetCanceled(cancellationToken));
+            using var u2 = mediator.Subscribe<T>((ev, ctx, ct) =>
+            {
+                if (filter?.Invoke(ev) ?? true)
+                    channel.Writer.TryWrite(ev);
+            
+                return Task.CompletedTask;
+            });
+            while (await channel.Reader.WaitToReadAsync(cancellationToken))
+            {
+                var item = await channel.Reader.ReadAsync(cancellationToken);
+                yield return item;
+            }
         }
     }
+
     
-    
+    // TODO: mediatorcontext does not have a subscribe
+    // /// <summary>
+    // /// Wait for event handler to fire
+    // /// </summary>
+    // /// <param name="mediator"></param>
+    // /// <param name="filter">Allows you to filter the event instead of completing</param>
+    // /// <param name="cancellationToken"></param>
+    // /// <typeparam name="T"></typeparam>
+    // /// <returns></returns>
+    // public static async Task<T> WaitForSingleEvent<T>(
+    //     this IMediatorContext context, 
+    //     Func<T, bool>? filter = null,
+    //     CancellationToken cancellationToken = default
+    // ) where T : IEvent
+    // {
+    //     var tcs = new TaskCompletionSource<T>();
+    //     await using var u1 = cancellationToken.Register(() => tcs.TrySetCanceled(cancellationToken));
+    //     using var u2 = mediator.Subscribe<T>((ev, ctx, ct) =>
+    //     {
+    //         if (filter?.Invoke(ev) ?? true)
+    //             tcs.TrySetResult(ev);
+    //         return Task.CompletedTask;
+    //     });
+    //
+    //     return await tcs.Task.ConfigureAwait(false);
+    // }
+
+
     /// <summary>
     /// This method unwraps the mediator context from an async enumerable result - useful for outgoing aspnet endpoints
     /// </summary>
