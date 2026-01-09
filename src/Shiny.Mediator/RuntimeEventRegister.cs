a7937c076ba39617ed57c0392165903adeea6da4
Allan Ritchie <allan.ritchie@gmail.com>
2026-01-09 09:46:47 -0500

    Add thread safety to RuntimeEventRegister
    Wrapped handler list operations in lock statements using a syncLock object to ensure thread safety when adding, removing, or retrieving event handlers.

--- src/Shiny.Mediator/RuntimeEventRegister.cs
+++ src/Shiny.Mediator/RuntimeEventRegister.cs
@@ -6,13 +6,29 @@ namespace Shiny.Mediator;
 public class RuntimeEventRegister : IEventCollector
 {
     readonly List<object> handlers = new();
+    readonly Lock syncLock = new();
     
     public void Add<TEvent>(IEventHandler<TEvent> handler) where TEvent : IEvent
-        => this.handlers.Add(handler);
+    {
+        lock (this.syncLock)
+        {
+            this.handlers.Add(handler);
+        }
+    }
     
     public void Remove<TEvent>(IEventHandler<TEvent> handler) where TEvent : IEvent
-        => this.handlers.Remove(handler);
+    {
+        lock (this.syncLock)
+        {
+            this.handlers.Remove(handler);
+        }
+    }
     
     public IReadOnlyList<IEventHandler<TEvent>> GetHandlers<TEvent>() where TEvent : IEvent
-        => this.handlers.OfType<IEventHandler<TEvent>>().ToList();
+    {
+        lock (this.syncLock)
+        {
+            return this.handlers.OfType<IEventHandler<TEvent>>().ToList();
+        }
+    }
 }
