e3deb8a8e0f3cf0142c7fe0ef6e097423437da1e
Allan Ritchie <allan.ritchie@gmail.com>
2026-04-28 12:55:31 -0400

    Beta3 fixes

--- src/Shiny.Mediator.MicrosoftAI.SourceGenerators/Constants.cs
+++
@@ -1,6 +0,0 @@
-namespace Shiny.Mediator.MicrosoftAI.SourceGenerators;
-
-public static class Constants
-{
-    public const string GeneratedCodeAttributeString = "[global::System.CodeDom.Compiler.GeneratedCodeAttribute(\"Shiny.Mediator.MicrosoftAI.SourceGenerators\", \"1.0.0\")]";
-}
